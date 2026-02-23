018ea1ef0876a219726d32e38f9b9ac2d2decd10
Adrian <adrianmespindola@gmail.com>
2026-02-23 11:57:50 -0300

    UPDATE: burguer menu, education fixed

--- app/sections/Education.tsx
+++ app/sections/Education.tsx
@@ -8,10 +8,10 @@ const Educacion: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center px-6">
       <h2 className="text-3xl font-bold mb-6">{t.educacion.title}</h2>
-      <ul className="space-y-4 text-gray-700 w-1/2 flex flex-col items-center">
-        <li>{t.educacion.analitycs}</li>
-        <li>{t.educacion.fullstack}</li>
-        <li>{t.educacion.java}</li>
+      <ul className="space-y-4 text-gray-700 md:w-2/3 flex flex-col items-center">
+        <li className="mb-5 border-b-2 w-full text-center">{t.educacion.analitycs}</li>
+        <li className="mb-5 border-b-2 w-full text-center">{t.educacion.fullstack}</li>
+        <li className="mb-5 border-b-2 w-full text-center">{t.educacion.java}</li>
       </ul>
     </div>
   );
